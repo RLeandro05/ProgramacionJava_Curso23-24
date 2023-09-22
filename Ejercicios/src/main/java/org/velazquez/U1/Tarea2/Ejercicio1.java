bb966149ed2efa1b5fae0c010bd46082bbba8659
RLeandro05 <lligpic577@educaand.es>
2023-09-22 10:45:15 +0200

    Añadida el Ejercicio 1 de la Tarea 2

---
+++ Ejercicios/src/main/java/org/velazquez/U1/Tarea2/Ejercicio1.java
@@ -0,0 +1,15 @@
+package org.velazquez.U1.Tarea2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+        System.out.print("El primer número es: ");
+        System.out.println(numero1);
+        System.out.print("El segundo número es: ");
+        System.out.println(numero2);
+
+        System.out.print("Por tanto, su producto es: ");
+        System.out.print(numero1 * numero2);
+    }
+}
