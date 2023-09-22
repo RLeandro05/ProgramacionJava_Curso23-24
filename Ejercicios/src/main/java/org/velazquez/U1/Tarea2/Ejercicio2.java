d72c991f423cfeff150c94d9d62760453125942d
RLeandro05 <lligpic577@educaand.es>
2023-09-22 10:49:48 +0200

    Añadido el Ejercicio 2 de la Tarea 2

---
+++ Ejercicios/src/main/java/org/velazquez/U1/Tarea2/Ejercicio2.java
@@ -0,0 +1,17 @@
+package org.velazquez.U1.Tarea2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        int numero1 = 10;
+        int numero2 = 5;
+
+        System.out.print("El primer número es: ");
+        System.out.println(numero1);
+        System.out.print("El segundo número es: ");
+        System.out.println(numero2);
+
+        System.out.print("Por tanto, su división resulta en: ");
+        System.out.print(numero1/numero2);
+    }
+}
