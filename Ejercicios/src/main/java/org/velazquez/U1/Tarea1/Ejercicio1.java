45be9027678dfbf168a1e51b17f6e2ab547de2d9
RLeandro05 <lligpic577@educaand.es>
2023-09-22 10:35:07 +0200

    Publicada la Tarea 1 de la Unidad 1

---
+++ Ejercicios/src/main/java/org/velazquez/U1/Tarea1/Ejercicio1.java
@@ -0,0 +1,8 @@
+package org.velazquez.U1.Tarea1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("¡Hola");
+        System.out.print("Leandro!");
+    }
+}
