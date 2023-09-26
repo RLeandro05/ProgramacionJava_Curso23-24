ca93916665c9ef91a3a638056bfaf7f2c34e77a8
RLeandro05 <lligpic577@educaand.es>
2023-09-26 09:31:07 +0200

    Modificado el Ejercicio 2 de Tarea 4

--- Ejercicios/src/main/java/org/velazquez/U1/Tarea4/Ejercicio2.java
+++ Ejercicios/src/main/java/org/velazquez/U1/Tarea4/Ejercicio2.java
@@ -2,11 +2,11 @@ package org.velazquez.U1.Tarea4;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        System.out.println("El alumno Manuel Ortega va a ser evaluado con su media final a partir de dos notas, un 7,1 y un 5,8.");
+        System.out.println("El alumno Manuel Ortega va a ser evaluado con su media final a partir de dos notas, un 7 y un 5.");
         System.out.println("Sabiendo que la primera nota vale un 40% y la segunda nota vale un 60%,");
 
-        double n1 = 7.1;
-        double n2 = 5.8;
+        double n1 = 7;
+        double n2 = 5;
 
         double multiplicacion1 = (double) (n1*0.4);
         double multiplicacion2 = (double) (n2*0.6);
