856e04e3e9ab8fe6476f7035c834ab2d3d0dfdff
RLeandro05 <lligpic577@educaand.es>
2023-10-06 09:18:08 +0200

    Añadido Ejercicio 1 de Tarea 6

---
+++ Ejercicios/src/main/java/org/velazquez/U1/Tarea6/Ejercicio1.java
@@ -0,0 +1,50 @@
+package org.velazquez.U1.Tarea6;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un mes en número: ");
+        int mes = sc.nextInt();
+
+        if (mes == 1) {
+            System.out.println("Enero");
+        }
+        if (mes == 2) {
+            System.out.println("Febrero");
+        }
+        if (mes == 3) {
+            System.out.println("Marzo");
+        }
+        if (mes == 4) {
+            System.out.println("Abril");
+        }
+        if (mes == 5) {
+            System.out.println("Mayo");
+        }
+        if (mes == 6) {
+            System.out.println("Junio");
+        }
+        if (mes == 7) {
+            System.out.println("Julio");
+        }
+        if (mes == 8) {
+            System.out.println("Agosto");
+        }
+        if (mes == 9) {
+            System.out.println("Septiembre");
+        }
+        if (mes == 10) {
+            System.out.println("Octubre");
+        }
+        if (mes == 11) {
+            System.out.println("Noviembre");
+        }
+        if (mes == 12) {
+            System.out.println("Diciembre");
+        }
+
+    }
+}
