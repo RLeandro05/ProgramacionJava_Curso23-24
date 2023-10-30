bb78d32884c7577ffa887d633399552d33affb52
RLeandro05 <lligpic577@g.educaand.es>
2023-10-30 20:40:41 +0100

    Arreglado un título de un ejercicio de prueba

--- Ejercicios/src/main/java/org/velazquez/U1/Pruebas/Prueba7.java
+++ Ejercicios/src/main/java/org/velazquez/U1/Pruebas/Prueba7.java
@@ -3,7 +3,7 @@ package org.velazquez.U1.Pruebas;
 public class Prueba7 {
     public static void main(String[] args) {
 
-        System.out.println("TRIÁNGULO INVERTIDO Y HUECO");
+        System.out.println("TRIÁNGULO INVERTIDO");
         System.out.println();
         int n = 5;
 
