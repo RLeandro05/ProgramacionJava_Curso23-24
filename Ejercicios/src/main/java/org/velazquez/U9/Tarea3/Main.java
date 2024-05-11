969dd0d6a43f373bf9232a2b9cb8caaae17f92d3
ligero <lligpic577@g.educaand.es>
2024-05-11 13:24:10 +0200

    Actualizacion

--- Ejercicios/src/main/java/org/velazquez/U9/Tarea3/Main.java
+++ Ejercicios/src/main/java/org/velazquez/U9/Tarea3/Main.java
@@ -11,11 +11,10 @@ public class Main {
 //            connection = DriverManager.getConnection("jdbc:sqlite:sampledatabase.db");
 //            Statement statement = connection.createStatement();
 //            statement.setQueryTimeout(30);
-//            ResultSet rs1 = statement.executeQuery("SELECT * from customers where customerNumber = 577");
+//            ResultSet rs1 = statement.executeQuery("SELECT * from orderdetails where orderNumber = 10100");
 //            while (rs1.next()) {
-//                System.out.println("Nombre de cliente: " + rs1.getString("customerName"));
-//                System.out.println("State del cliente: " + rs1.getString("state"));
-//                System.out.println("Número de cliente: " + rs1.getInt("customerNumber"));
+//                System.out.println("Número de pedido: " + rs1.getInt("orderNumber"));
+//                System.out.println("Número de producto: " + rs1.getString("productCode"));
 //            }
 //        } catch (SQLException e) {
 //            System.err.println(e.getMessage());
@@ -45,8 +44,10 @@ public class Main {
                 asignarEmpleadoaCliente();
                 break;
             case 3:
+                asignarProductoaPedidos();
                 break;
             case 4:
+                mostrarDetallesPedido();
                 break;
             case 5:
                 System.out.println("Saliste!");
@@ -168,7 +169,13 @@ public class Main {
                 if (rs2.next()) {
                     System.out.println("El cliente con id '"+idCliente+"' existe.");
 
+                    String sql3 = "Update customers set salesRepEmployeeNumber = "+idEmpleado+" where customerNumber = "+idCliente;
 
+                    PreparedStatement statement3 = connection.prepareStatement(sql3);
+
+                    int numFilas = statement3.executeUpdate();
+
+                    System.out.println(numFilas+" filas actualizadas");
                 } else {
                     System.out.println("El cliente con id '"+idCliente+"' no existe.");
                 }
@@ -185,6 +192,129 @@ public class Main {
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    public static void asignarProductoaPedidos() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca el identificador del producto: ");
+        String idProducto = sc.next();
+
+        System.out.println("Introduzca el identificador del pedido: ");
+        int idPedido = sc.nextInt();
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:sampledatabase.db");
+
+            String sql1 = "Select productCode from products where upper(productCode) like '"+idProducto+"'";
+
+            PreparedStatement statement1 = connection.prepareStatement(sql1);
+
+            ResultSet rs1 = statement1.executeQuery();
+
+            if (rs1.next()) {
+                System.out.println("El producto con id '"+idProducto+"' existe.");
+                String sql2 = "Select orderNumber from orderdetails where orderNumber = "+idPedido;
+
+                PreparedStatement statement2 = connection.prepareStatement(sql2);
+
+                ResultSet rs2 = statement2.executeQuery();
+
+                if (rs2.next()) {
+                    System.out.println("El pedido con id '"+idPedido+"' existe.");
+
+                    System.out.println("Introduzca el número de unidades: ");
+                    int quantityOrdered = sc.nextInt();
+
+                    System.out.println("Introduzca el precio por cada unidad: ");
+                    double priceEach = sc.nextDouble();
+
+                    System.out.println("Introduzca el número de línea de orden: ");
+                    int orderLineNumber = sc.nextInt();
+
+                    String sql3 = "Insert into orderdetails (orderNumber, productCode, quantityOrdered, priceEach, orderLineNumber) values (?, ?, ?, ?, ?)";
+
+                    PreparedStatement statement3 = connection.prepareStatement(sql3);
+
+                    statement3.setInt(1, idPedido);
+                    statement3.setString(2, idProducto);
+                    statement3.setInt(3, quantityOrdered);
+                    statement3.setDouble(4, priceEach);
+                    statement3.setInt(5, orderLineNumber);
+
+                    statement3.executeUpdate();
+
+                    System.out.println("Datos del producto con número "+idProducto+" insertados correctamente al pedido con número "+idPedido);
+                } else {
+                    System.out.println("El pedido con id '"+idPedido+"' no existe.");
+                }
+            } else {
+                System.out.println("El producto con id '"+idProducto+"' no existe.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void mostrarDetallesPedido() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca el código del pedido: ");
+        int orderNumber = sc.nextInt();
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:sampledatabase.db");
+
+            String verificarProducto = "Select * from orders where orderNumber = "+orderNumber;
+
+            PreparedStatement pst = connection.prepareStatement(verificarProducto);
+
+            ResultSet verificacion = pst.executeQuery();
+
+            if (verificacion.next()) {
+                System.out.println("El pedido con código '"+orderNumber+"' existe. A continuación, sus detalles: ");
+
+                String sqlPRODUCTOSDELCODIGO = "Select * from products p join orderdetails od on od.productCode = p.productCode where orderNumber = "+orderNumber;
+
+                PreparedStatement mostrarProductos = connection.prepareStatement(sqlPRODUCTOSDELCODIGO);
+
+                ResultSet RSmostrarProductos = mostrarProductos.executeQuery();
+
+                double total;
+                while (RSmostrarProductos.next()) {
+                    System.out.println("Nombre del Producto: "+RSmostrarProductos.getString("productName"));
+                    System.out.println("Cantidad de unidades: "+RSmostrarProductos.getInt("quantityOrdered"));
+                    System.out.println("Precio por unidad: "+RSmostrarProductos.getDouble("priceEach"));
+                    total = RSmostrarProductos.getInt("quantityOrdered")*RSmostrarProductos.getDouble("priceEach");
+                    System.out.println("Total: "+total+"€");
+                    System.out.println("------------------------------------------------------------------------------");
+                }
+
+            } else {
+                System.out.println("El pedido con código '"+orderNumber+"' no existe.");
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        } finally {
+            try {
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.getMessage();
+            }
+
         }
     }
 }
