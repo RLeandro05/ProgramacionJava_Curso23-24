8950b0908bd677637b0c9194ac8291acac1aeaca
RLeandro05 <lligpic577@g.educaand.es>
2024-05-13 10:25:05 +0200

    Entregable U9

---
+++ Ejercicios/src/main/java/org/velazquez/U9/Ligero_Picon_U9_Entregable/ConexionDB.java
@@ -0,0 +1,42 @@
+package org.velazquez.U9.Ligero_Picon_U9_Entregable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static Connection con = null;
+
+    public ConexionDB() {
+    }
+    public static Connection getConnection(){
+
+        if (con == null) {
+            try{
+//                String driver="com.mysql:mysql-connector-j:8.3.0";
+                String url="jdbc:mysql://localhost:3336/classicmodels?user=root&password=XXXXX";
+                String pwd="XXXXX";
+                String usr="root";
+                con = DriverManager.getConnection(url,usr,pwd); // Lo conectamos al driver
+                System.out.println("Conexión establecida.");
+            }
+            catch(SQLException ex){
+                System.out.println(ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+        return con; // Devolvemos la conexión
+    }
+
+    public static void close() {
+        try {
+            if (con != null) {
+                con.close();
+                con = null;
+                System.out.println("Conexión cerrada correctamente.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Hubo errores en: "+e.getMessage());
+        }
+    }
+}
---
+++ Ejercicios/src/main/java/org/velazquez/U9/Ligero_Picon_U9_Entregable/Main.java
@@ -0,0 +1,26 @@
+package org.velazquez.U9.Ligero_Picon_U9_Entregable;
+
+public class Main {
+    public static void main(String[] args) {
+//
+//        Consultas consulta = new Consultas();
+//
+//        consulta.empleadosOficinaMayor();
+//
+//        System.out.println("--------------------------------------");
+//
+//        consulta.productosNoVendidos();
+//
+//        System.out.println("--------------------------------------");
+//
+//        consulta.informeCategoria();
+
+        Transacciones transaccion = new Transacciones();
+
+        transaccion.insertarClienteyDosPagos();
+
+//        System.out.println("--------------------------------------");
+//
+//        transaccion.insertarOficinaTokio();
+    }
+}
