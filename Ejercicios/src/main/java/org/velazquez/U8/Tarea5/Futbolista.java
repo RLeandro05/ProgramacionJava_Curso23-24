b83bfa0d302b40d3cdb9901e2afeb5f5a95fb86d
ligero <lligpic577@g.educaand.es>
2024-05-17 09:36:37 +0200

    Actualizado

---
+++ Ejercicios/src/main/java/org/velazquez/U8/Tarea5/Futbolista.java
@@ -0,0 +1,61 @@
+package org.velazquez.U8.Tarea5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Futbolista {
+
+    private int dorsalFutbolista;
+    private String nombreFutbolista;
+    private List<String> listaDermacaciones = new ArrayList<>();
+    private String equipoFutbolista;
+
+    public Futbolista(int dorsalFutbolista, String nombreFutbolista, List<String> listaDermacaciones, String equipoFutbolista) {
+        this.dorsalFutbolista = dorsalFutbolista;
+        this.nombreFutbolista = nombreFutbolista;
+        this.listaDermacaciones = listaDermacaciones;
+        this.equipoFutbolista = equipoFutbolista;
+    }
+
+    public int getDorsalFutbolista() {
+        return dorsalFutbolista;
+    }
+
+    public void setDorsalFutbolista(int dorsalFutbolista) {
+        this.dorsalFutbolista = dorsalFutbolista;
+    }
+
+    public String getNombreFutbolista() {
+        return nombreFutbolista;
+    }
+
+    public void setNombreFutbolista(String nombreFutbolista) {
+        this.nombreFutbolista = nombreFutbolista;
+    }
+
+    public List<String> getListaDermacaciones() {
+        return listaDermacaciones;
+    }
+
+    public void setListaDermacaciones(List<String> listaDermacaciones) {
+        this.listaDermacaciones = listaDermacaciones;
+    }
+
+    public String getEquipoFutbolista() {
+        return equipoFutbolista;
+    }
+
+    public void setEquipoFutbolista(String equipoFutbolista) {
+        this.equipoFutbolista = equipoFutbolista;
+    }
+
+    @Override
+    public String toString() {
+        return "Futbolista{" +
+                "dorsalFutbolista=" + dorsalFutbolista +
+                ", nombreFutbolista='" + nombreFutbolista + '\'' +
+                ", listaDermacaciones=" + listaDermacaciones +
+                ", equipoFutbolista='" + equipoFutbolista + '\'' +
+                '}';
+    }
+}
---
+++ Ejercicios/src/main/java/org/velazquez/U8/Tarea5/Main.java
@@ -0,0 +1,62 @@
+package org.velazquez.U8.Tarea5;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Futbolista> listaFutbolistas = new ArrayList<>();
+
+        Futbolista f1 = new Futbolista(1, "Casillas", List.of(new String[]{"Portero"}), "Real Madrid");
+        Futbolista f2 = new Futbolista(15, "Ramos", List.of(new String[]{"Lateral derecho", "Medio centro"}), "Real Madrid");
+        Futbolista f3 = new Futbolista(3, "Pique", List.of(new String[]{"Central"}), "FC Barcelona");
+        Futbolista f4 = new Futbolista(5, "Puyol", List.of(new String[]{"Central"}), "FC Barcelona");
+        Futbolista f5 = new Futbolista(11, "Capdevila", List.of(new String[]{"Lateral izquierdo"}), "Villareal");
+        Futbolista f6 = new Futbolista(14, "Xabi Alonso", List.of(new String[]{"Defensa mediocampo", "Mediocampo"}), "Real Madrid");
+        Futbolista f7 = new Futbolista(16, "Busquets", List.of(new String[]{"Defensa mediocampo"}), "FC Barcelona");
+        Futbolista f8 = new Futbolista(8, "Xavi Hernandez", List.of(new String[]{"Mediocampo"}), "FC Barcelona");
+        Futbolista f9 = new Futbolista(18, "Pedrito", List.of(new String[]{"Extremo izquierdo", "Falso extremo"}), "FC Barcelona");
+        Futbolista f10 = new Futbolista(6, "Iniesta", List.of(new String[]{"Extremo derecho", "Mediocampo"}), "FC Barcelona");
+        Futbolista f11 = new Futbolista(7, "Villa", List.of(new String[]{"Delantero centro"}), "FC Barcelona");
+
+        listaFutbolistas.add(f1);
+        listaFutbolistas.add(f2);
+        listaFutbolistas.add(f3);
+        listaFutbolistas.add(f4);
+        listaFutbolistas.add(f5);
+        listaFutbolistas.add(f6);
+        listaFutbolistas.add(f7);
+        listaFutbolistas.add(f8);
+        listaFutbolistas.add(f9);
+        listaFutbolistas.add(f10);
+        listaFutbolistas.add(f11);
+
+        Gson gson = new Gson();
+
+        String jsonListaFutbolistas = gson.toJson(listaFutbolistas);
+
+        System.out.println(jsonListaFutbolistas);
+
+        System.out.println("----------------------------------------------");
+
+        List<Futbolista> newListaFutbolistas = gson.fromJson(jsonListaFutbolistas, listaFutbolistas.getClass());
+
+        System.out.println(newListaFutbolistas);
+
+        System.out.println("----------------------------------------------");
+
+        List<String> listaDemarcaciones = new ArrayList<>();
+
+        for (Futbolista futbolista : listaFutbolistas) {
+            if (futbolista.getNombreFutbolista().equals("Ramos")) {
+                listaDemarcaciones.addAll(futbolista.getListaDermacaciones());
+            }
+        }
+
+        System.out.println(listaDemarcaciones);
+    }
+}
