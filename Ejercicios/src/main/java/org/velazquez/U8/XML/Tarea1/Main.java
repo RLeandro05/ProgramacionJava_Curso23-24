27c82c921a2ae58ee5566e78ca817ad3e80d4d4d
ligero <lligpic577@g.educaand.es>
2024-05-19 14:12:12 +0200

    Actualizado

--- Ejercicios/src/main/java/org/velazquez/U8/XML/Tarea1/Main.java
+++ Ejercicios/src/main/java/org/velazquez/U8/XML/Tarea1/Main.java
@@ -3,6 +3,6 @@ package org.velazquez.U8.XML.Tarea1;
 public class Main {
     public static void main(String[] args) {
 
-        ParseadorDOMPersonas.parse("C:\\Users\\liger\\Desktop\\programacion_23_24\\Ejercicios\\src\\main\\java\\org\\velazquez\\U8\\XML\\Tarea1\\Factura (LM).xml");
+        ParseadorDOMPersonas.parse("C:\\Users\\liger\\Desktop\\programacion_23_24\\Ejercicios\\src\\main\\java\\org\\velazquez\\U8\\XML\\Tarea1\\facturasXML.xml");
     }
 }
--- Ejercicios/src/main/java/org/velazquez/U8/XML/Tarea1/ParseadorDOMPersonas.java
+++ Ejercicios/src/main/java/org/velazquez/U8/XML/Tarea1/ParseadorDOMPersonas.java
@@ -33,9 +33,63 @@ public class ParseadorDOMPersonas {
                 System.out.println("Parece que no hay etiquetas con el nombre '"+etiqueta+"'.");
                 System.out.println("Escriba otra: ");
                 etiqueta = sc.next();
+                listaNodos = doc.getElementsByTagName(etiqueta);
             }
 
             System.out.println("Al parecer, esta etiqueta está presente un número de '"+listaNodos.getLength()+"' veces.");
+
+            System.out.println("------------------------------------------------------------------------------");
+
+            System.out.println("Vamos a recorrer las etiquetas con dicho nombre.");
+
+            for (int i = 0; i < listaNodos.getLength(); i++) {
+                Node nodo = listaNodos.item(i);
+
+                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elemento = (Element) listaNodos.item(i);
+                    System.out.println("Estamos en un elemento, el cual es la etiqueta: '"+elemento.getTagName()+"'.");
+
+                    System.out.println("------------------------------------------------------------------------------");
+
+                    System.out.println("Vamos a ver si tiene elementos hijos...");
+                    System.out.println("Escaneando....");
+
+                    NodeList listaHijos = nodo.getChildNodes();
+
+                    if (elemento.hasChildNodes()) {
+                        System.out.println("El elemento '"+elemento.getTagName()+"' tiene "+listaHijos.getLength()+" hijos.");
+                    }
+
+                    System.out.println("------------------------------------------------------------------------------");
+
+                    if (listaNodos.getLength() > 0) {
+                        System.out.println("Veamos uno a uno los elementos hijos de la etiqueta '"+elemento.getTagName()+"'.");
+
+                        for (int j = 0; j < listaHijos.getLength(); j++) {
+
+                            if (listaHijos.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                                Element elementoHijo = (Element) listaHijos.item(j);
+                                System.out.println("Nos encontramos con el elemento hijo '"+elementoHijo.getTagName()+"'.");
+                                if (elementoHijo.hasChildNodes()) {
+
+                                    NodeList listaHijosHijos = listaHijos.item(j).getChildNodes();
+
+                                    for (int k = 0; k < listaHijosHijos.getLength(); k++) {
+                                        if (listaHijosHijos.item(k).getNodeType() == Node.ELEMENT_NODE) {
+                                            Element elementoHijoHijo = (Element) listaHijosHijos.item(k);
+                                            System.out.println("Tenemos el siguiente elemento de los hijos del elemento '"+elementoHijo.getTagName()+"': '"+elementoHijoHijo.getTagName()+" - "+elementoHijoHijo.getTextContent()+"'");
+                                        }
+                                    }
+                                } else {
+                                    System.out.println("Siendo su valor de: '"+elementoHijo.getTextContent()+"'.");
+                                }
+
+                                System.out.println("------------------------------------------------------------------------------");
+                            }
+                        }
+                    }
+                }
+            }
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             for (StackTraceElement element : e.getStackTrace()) {
